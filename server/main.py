bfed05cea28a1f59903b49ef7035c3a691067b63
yuki-oka <55942183+fate7bardiche@users.noreply.github.com>
2025-02-13 19:41:21 +0900

    feat: 複数のクライアントと接続できるように、サーバーをマルチスレッド化する

--- server/main.py
+++ server/main.py
@@ -1,25 +1,35 @@
 import socket
 import json
+import threading
 from server_socket import ServerSocket
 from rpc_access_controller import RpcAccessController
+
+def loop_handler(connection: socket):
+     rpc_access_controller  = RpcAccessController()
+     while True:
+        print("==============================")
+        rpc_access_controller.receive_response(connection)
+        print("==============================")
+
 
 def main():
     config = json.load(open('../config.json'))
     socket_address_path =  "../" + config["filepath"]
-    server_soccket = ServerSocket(socket_address_path)
+    server_soccket = ServerSocket(socket_address_path, 5)
 
     server_soccket.start_settion()
 
+    clients = []
     while True:
         connection = server_soccket.accept_client()
-        rpc_access_controller  = RpcAccessController()
-
         print("接続しました。")
+        
         try:
-            while True:
-              print("==============================")
-              rpc_access_controller.receive_response(connection)
-              print("==============================")
+           clients.append(connection)
+           print(f"現在{len(clients)}個のクライアントと接続しています。")
+
+           thread = threading.Thread(target=loop_handler, args=(connection, ), daemon=True)
+           thread.start()
         except socket.error as err:
            print(err)
            break
--- server/server_socket.py
+++ server/server_socket.py
@@ -2,20 +2,22 @@ import socket
 import os
 
 class ServerSocket:
-    def __init__ (self, socket_address_path: str):
+
+    def __init__ (self, socket_address_path: str, backlog_num: int):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.socket_address_path = socket_address_path
+        self.backlog_num = backlog_num
 
     def start_settion(self):
         socket_address_path = self.socket_address_path
-
+        
         try:
             os.unlink(socket_address_path)
         except:
             pass
 
         self.sock.bind(socket_address_path)
-        self.sock.listen(1)
+        self.sock.listen(self.backlog_num)
         print("接続待機中です。")
 
     def accept_client(self):
